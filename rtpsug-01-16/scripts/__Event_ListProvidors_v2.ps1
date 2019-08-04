61f18320a2548fd60e65eabc40ed9c2ad451b8b3
PBossman <12040309+pbossman@users.noreply.github.com>
2019-08-03 22:39:17 -0400

    Revert "rtpsug-01-16 presentation files"
    This reverts commit 2a7e9930ed910fffda4416e7281a315b21ff70da.

--- rtpsug-01-16/scripts/__Event_ListProvidors_v2.ps1
+++
@@ -1,3 +0,0 @@
-### Reference
-## https://blogs.technet.microsoft.com/ashleymcglone/2015/08/31/forensics-automating-active-directory-account-lockout-search-with-powershell-an-example-of-deep-xml-filtering-of-event-logs-across-multiple-servers-in-parallel/
-###
